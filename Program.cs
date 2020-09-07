acc139800a81012d144250312854cccdb2c4466e
Salman Ali Banani <salman.banani@purple.telstra.com>
2020-09-07 15:55:12 +1000

    First commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureServiceBusTopicReceive
+{
+    class Program
+    {
+        static ISubscriptionClient client;
+
+        static async Task Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+          
+            var connectionString = config["connectionString"];
+            client = new SubscriptionClient(connectionString, "demotopic4699", "demotopicsubscription"); 
+
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            // Register the function that processes messages.
+            client.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+
+            Console.WriteLine("Waiting for a message.  Type q to quit.");
+            Console.ReadKey();
+            await client.CloseAsync();
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: SequenceNumber: {message.SystemProperties.SequenceNumber} \nBody: {Encoding.UTF8.GetString(message.Body)}");
+            await client.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# AzureServiceBusTopicReceive
+
+Code sample.  See the accompanied blog <a href="https://salmanalibanani.com/2020/09/04/azure-service-bus-client-library-for-.net/">here</a>.
